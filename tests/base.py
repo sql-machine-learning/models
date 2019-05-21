1cf8930611e039fe23755b126882ae7afa91cf1f
typhoonzero <typhoonzero1986@gmail.com>
2019-05-21 20:12:20 +0800

    fix tests

--- tests/base.py
+++ tests/base.py
@@ -12,16 +12,14 @@ def prepare_dataset():
 
 
 class BaseTestCases:
-    class BaseTest(unittest.TestCase):
-        def __init__(self):
-            super(BaseTestCases.BaseTest, self).__init__()
-            self.model, self.features, self.label = None, None, None
+    class BaseTest(object):
+        def setUp(self):
+            self.model, self.features, self.label = None, {}, None
 
         def test_train_and_predict(self):
+            self.setUp()
             train_input_fn(self.features, self.label)
 
-            self
             print('Calling BaseTest:testCommon')
             value = 5
-            self.assertEquals(value, 5)
-
+            assert(value == 5)
