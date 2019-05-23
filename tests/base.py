77d1b58690e120d69a5f8d9114e0d54fa66b6554
Wu Yi <xiongmu.wy@antfin.com>
2019-05-23 10:44:30 +0800

    Keras models support (#7)
    * temp add
    
    * fix tests
    
    * update
    
    * addback
    
    * update
    
    * fit iris test case
    
    * follow comments

---
+++ tests/base.py
@@ -0,0 +1,31 @@
+import unittest
+import tensorflow as tf
+
+def train_input_fn(features, labels, batch_size=32):
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    return dataset
+
+def eval_input_fn(features, labels, batch_size=32):
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+class BaseTestCases:
+    class BaseTest(object):
+        def setUp(self):
+            self.model, self.features, self.label = None, {}, None
+
+        def test_train_and_predict(self):
+            self.setUp()
+
+            self.model.compile(optimizer=self.model.default_optimizer(),
+                loss=self.model.default_loss(),
+                metrics=["accuracy"])
+            self.model.fit(train_input_fn(self.features, self.label),
+                epochs=self.model.default_training_epochs(),
+                steps_per_epoch=100, verbose=0)
+            loss, acc = self.model.evaluate(eval_input_fn(self.features, self.label))
+            print(loss, acc)
+            assert(loss < 10)
+            assert(acc > 0.3)
