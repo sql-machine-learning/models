d620cc457ac9a068fb304715fba8c5d80b3017f7
alfredchenxiang <alfredchenxiang@didiglobal.com>
2019-09-25 08:07:39 +0800

    Update : Rename variables.

--- sqlflow_models/deep_embedding_cluster.py
+++ sqlflow_models/deep_embedding_cluster.py
@@ -240,13 +240,13 @@ class DeepEmbeddingClusterModel(keras.Model):
                     print('Early stopping since delta_table {} has reached tol {}'.format(delta_percentage, self._tol))
                     break
             idx = index_array[index * self._train_batch_size: min((index + 1) * self._train_batch_size, record_num)]
-            other_input = all_records_ndarray[idx]
-            input_dict = {}
+            batch_input = all_records_ndarray[idx]
+            batch_input_dict = {}
             for i in range(self._feature_columns_dims):
                 name = 'col_%d' % i
-                input_dict[name] = pd.Series(other_input[:,i])
+                batch_input_dict[name] = pd.Series(batch_input[:,i])
 
-            loss = self.train_on_batch(x=input_dict, y=p[idx])
+            loss = self.train_on_batch(x=batch_input_dict, y=p[idx])
             if ite % 100 == 0:
                 print('{} Training at iter:{} -> loss:{}.'.format(datetime.now(), ite, loss))
             index = index + 1 if (index + 1) * self._train_batch_size <= record_num else 0  # Update index
