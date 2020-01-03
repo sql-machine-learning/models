7df607970a9ad44d810e64c2e61ed9686a076bad
zhuwenjing <zhuwenjing@didiglobal.com>
2020-01-03 10:39:44 +0800

    update

--- sqlflow_models/__init__.py
+++ sqlflow_models/__init__.py
@@ -1,6 +1,5 @@
 from ._version import __version__
 from .dnnclassifier import DNNClassifier
-from .dnnregressor import DNNRegressor
 from .lstmclassifier import StackedBiLSTMClassifier
 from .deep_embedding_cluster import DeepEmbeddingClusterModel
 from .dnnclassifier_functional_api_example import dnnclassifier_functional_model
