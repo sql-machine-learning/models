b9e43ea3f6df991716691485e41bdbb17582214e
Yancey1989 <yancey1989@gmail.com>
2020-04-10 22:48:37 +0800

    update

--- sqlflow_models/score_card.py
+++ sqlflow_models/score_card.py
@@ -10,7 +10,7 @@ import pandas as pd
 import scipy.stats.stats as stats
 import sklearn
 from sklearn.linear_model import LogisticRegression
-
+import pickle
 
 def optimizer():
     return SGD(lr=0.1, momentum=0.9)
@@ -25,16 +25,18 @@ class MyScoreCard(keras.Model):
 
         self._factor = 20/np.log(2)
         self._offset = 600 - 20*np.log(20) / np.log(2)
+        self._lr = LogisticRegression()
+        self._is_first_predict_batch = False
+        self._bins = dict()
 
     def call(self):
         pass
         
-    def _mono_bin(self, y, x, n=10):
+    def _pr_bin(self, y, x, n=10):
         # population frequency
-        r = 0
         bad_num = y.sum()
         good_num = y.count() - y.sum()
-        d1 = pd.DataFrame({'x':x,'y':y,'bucket':pd.qcut(x,n, duplicates='drop')})
+        d1 = pd.DataFrame({'x': x,'y': y,'bucket': pd.qcut(x, n, duplicates='drop')})
         d2 = d1.groupby('bucket',as_index=True)
         d3 = pd.DataFrame(d2.x.min(),columns=['min_bin']) 
 
@@ -56,32 +58,21 @@ class MyScoreCard(keras.Model):
     def _to_dataframe(self, dataset):
         x_df = pd.DataFrame()
         y_df = pd.DataFrame()
-
+        
         for features, label in dataset:
             dx = {}
             dy = {}
             for name, value in features.items():
                 dx[name] = value.numpy()[0]
-            dy['label'] = label.numpy()[0][0]
             x_df = x_df.append(dx, ignore_index=True)
-            y_df = y_df.append(dy, ignore_index=True)
+            if label is not None:
+                dy['label'] = label.numpy()[0][0]
+                y_df = y_df.append(dy, ignore_index=True)
         return x_df, y_df
 
     def _replace_woe(self, x, cut, woe):
-        return pd.cut(x, cut, labels=pd.Categorical(woe))
-
-    def _calsumscore(self, woe_list, coe):
-        n = coe.shape[1]
-        serise = 0
-        for i in range(n):
-            serise += coe[0][i] * np.array(woe_list.iloc[:, i])
-        score = serise * self._factor + self._offset
-        return score
-    def _get_score(self, coe, woe):
-        scores = []
-        for w in woe:
-            scores.append(round(coe * w * self._factor, 0))
-        return scores
+        #return pd.cut(x, cut, labels=pd.Categorical(woe))
+        return pd.cut(x, cut, labels=woe)
 
     def sqlflow_train_loop(self, x, epochs=1, verbose=0):
         ite = make_one_shot_iterator(x)
@@ -90,26 +81,42 @@ class MyScoreCard(keras.Model):
         x_df, y_df = self._to_dataframe(x)
 
         x_train_dict = {}
-        woe_dict = {}
         for col in x_df.columns:
-            if col in ['id', 'number_of_dependents']:
-                continue
-            fx1, cut1, x1_woe, iv1 = self._mono_bin(y_df['label'], x_df[col])
-            woe_dict[col] = fx1
-            x_replaced_woe = self._replace_woe(x_df[col], cut1, x1_woe)
+            dfx, cut, woe, iv = self._pr_bin(y_df['label'], x_df[col])
+            self._bins[col] = (dfx, cut, woe, iv)
+          
+            x_replaced_woe = self._replace_woe(x_df[col], cut, woe)
             x_train_dict[col] = x_replaced_woe
+
         x_train = pd.DataFrame.from_dict(x_train_dict)
-        clf = LogisticRegression()
-        clf.fit(x_train, y_df['label'])
-        clf.predict_proba(x_train)
-        coe = clf.coef_
-        scores = self._calsumscore(x_train, coe)
-        col_i = -1
-        for i in range(len(x_df.columns)):
-            col_name = x_df.columns[i]
-            if col_name in ['id', 'number_of_dependents']:
-                continue
-            col_i += 1
-            col_woe = woe_dict[col_name]
-            for j, w in enumerate(col_woe['woe']):
-                print(col_name, col_woe['woe'].index.to_list()[j], round(coe[0][col_i] * w * self._factor, 0))
+        self._lr.fit(x_train, y_df['label'])
+        coe = self._lr.coef_
+
+        for i, col_name in enumerate(x_df.columns):
+            bin_cols = self._bins[col_name][0].index.to_list()
+            for j, w in enumerate(self._bins[col_name][2]):
+                print(col_name, bin_cols[j], round(coe[0][i] * w * self._factor, 0))
+
+    def save_weights(self, save="", save_format="h5"):
+        pickle.dump(self._lr, open(save, 'wb'))
+        pickle.dump(self._bins, open(save+"_bin", 'wb'))
+
+    def load_weights(self, save):
+        self._lr = pickle.load(open(save, 'rb'))
+        self._bins = pickle.load(open(save+"_bin", 'rb'))
+
+    def predict_on_batch(self, features):
+        if not self._is_first_predict_batch:
+            self._is_first_predict_batch = True
+            return None
+        x_df, _ = self._to_dataframe([(features, None)])
+        x_train_dict = {}
+        for col in x_df.columns:
+            bin = self._bins[col]
+            x_train_dict[col] = self._replace_woe(x_df[col], bin[1], bin[2])
+        r = self._lr.predict_proba(pd.DataFrame.from_dict(x_train_dict))
+        return r
+
+def prepare_prediction_column(prediction):
+    """Return the class label of highest probability."""
+    return prediction.argmax(axis=-1)
