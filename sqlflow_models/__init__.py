3ce1221cbb31991ffd457e516e7ab034f513c4b9
Dingsu Wang <dingsu.wang@gmail.com>
2020-06-09 14:46:38 +0800

    Add GRU classifier and GRU time series model (#70)
    * Add GRU
    
    * Update RNN models
    
    * Add model type description

--- sqlflow_models/__init__.py
+++ sqlflow_models/__init__.py
@@ -1,8 +1,8 @@
 from ._version import __version__
 from .dnnclassifier import DNNClassifier
 from .dnnregressor import DNNRegressor
-from .lstmclassifier import StackedBiLSTMClassifier
+from .rnnclassifier import StackedRNNClassifier
 from .deep_embedding_cluster import DeepEmbeddingClusterModel
 from .dnnclassifier_functional_api_example import dnnclassifier_functional_model
-from .lstm_based_time_series_beta0 import LSTMBasedTimeSeriesModel
+from .rnn_based_time_series import RNNBasedTimeSeriesModel
 from .auto_estimator import AutoClassifier, AutoRegressor
--- sqlflow_models/rnnclassifier.py
+++ sqlflow_models/rnnclassifier.py
@@ -2,32 +2,38 @@ import tensorflow as tf
 
 _loss = ''
 
-class StackedBiLSTMClassifier(tf.keras.Model):
-    def __init__(self, feature_columns=None, stack_units=[32], hidden_size=64, n_classes=2):
-        """StackedBiLSTMClassifier
+class StackedRNNClassifier(tf.keras.Model):
+    def __init__(self, feature_columns=None, stack_units=[32], hidden_size=64, n_classes=2, model_type='rnn', bidirectional=False):
+        """StackedRNNClassifier
         :param feature_columns: All columns must be embedding of sequence column with same sequence_length.
         :type feature_columns: list[tf.embedding_column].
-        :param stack_units: Units for LSTM layer.
+        :param stack_units: Units for RNN layer.
         :type stack_units: vector of ints.
         :param n_classes: Target number of classes.
         :type n_classes: int.
+        :param model_type: Specific RNN model to be used, which can be chose from: ('rnn', 'lstm' and 'gru').
+        :type model_type: string.
+        :param bidirectional: Whether to use bidirectional or not.
+        :type bidirectional: bool.
         """
         global _loss
-        super(StackedBiLSTMClassifier, self).__init__()
+        super(StackedRNNClassifier, self).__init__()
 
+        self.models = {'rnn':tf.keras.layers.SimpleRNN, 'lstm':tf.keras.layers.LSTM, 'gru':tf.keras.layers.GRU}
+        self.bidirectionals = {True: tf.keras.layers.Bidirectional, False: lambda x: x}
         self.feature_layer = None
         if feature_columns is not None:
             self.feature_layer = tf.keras.experimental.SequenceFeatures(feature_columns)
-        self.stack_bilstm = []
+        self.stack_rnn = []
         self.stack_size = len(stack_units)
         self.stack_units = stack_units
         self.n_classes = n_classes
         if self.stack_size > 1:
             for i in range(self.stack_size - 1):
-                self.stack_bilstm.append(
-                    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.stack_units[i], return_sequences=True))
+                self.stack_rnn.append(
+                    self.bidirectionals[bidirectional](self.models[model_type.lower()](self.stack_units[i], return_sequences=True))
                 )
-        self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.stack_units[-1]))
+        self.rnn = self.bidirectionals[bidirectional](self.models[model_type.lower()](self.stack_units[-1]))
         self.hidden = tf.keras.layers.Dense(hidden_size, activation='relu')
         if self.n_classes == 2:
             # special setup for binary classification
@@ -48,8 +54,8 @@ class StackedBiLSTMClassifier(tf.keras.Model):
         seq_mask = tf.sequence_mask(seq_len)
         if self.stack_size > 1:
             for i in range(self.stack_size - 1):
-                x = self.stack_bilstm[i](x, mask=seq_mask)
-        x = self.lstm(x, mask=seq_mask)
+                x = self.stack_rnn[i](x, mask=seq_mask)
+        x = self.rnn(x, mask=seq_mask)
         x = self.hidden(x)
         return self.pred(x)
 
