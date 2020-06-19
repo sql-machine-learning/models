e2370d29f268e24732a62c484c7434f86523a7bb
Wu Yi <xiongmu.wy@antfin.com>
2020-06-19 18:51:03 +0800

    fix pai statsmodels version conflict (#73)

--- sqlflow_models/__init__.py
+++ sqlflow_models/__init__.py
@@ -1,3 +1,4 @@
+import traceback
 from ._version import __version__
 from .dnnclassifier import DNNClassifier
 from .dnnregressor import DNNRegressor
@@ -7,4 +8,9 @@ from .dnnclassifier_functional_api_example import dnnclassifier_functional_model
 from .rnn_based_time_series import RNNBasedTimeSeriesModel
 from .auto_estimator import AutoClassifier, AutoRegressor
 from .native_keras import RawDNNClassifier
-from .arima_with_stl_decomposition import ARIMAWithSTLDecomposition
+try:
+    # NOTE: statsmodels have version conflict on PAI
+    from .arima_with_stl_decomposition import ARIMAWithSTLDecomposition
+except:
+    print("model ARIMAWithSTLDecomposition is not imported")
+    traceback.print_exc()
