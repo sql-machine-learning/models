88717ab3d8a72cbf0f0b4eddd848c35135349f7c
brightcoder01 <bright.gong@outlook.com>
2020-08-10 19:50:47 +0800

    Move db utils method to db_adapter.py

--- runnables/extract_ts_features.py
+++ runnables/extract_ts_features.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd 
 from sqlalchemy import create_engine
 from time_series_processing.ts_feature_extractor import add_features_extracted_from_ts_data
+from run_io.db_adapter import convertDSNToRfc1783
 
 
 def build_argument_parser():
@@ -28,7 +29,8 @@ if __name__ == "__main__":
     output = os.getenv("SQLFLOW_TO_RUN_INTO")
     datasource = os.getenv("SQLFLOW_DATASOURCE")
 
-    engine = create_engine("{}/{}".format(datasource, args.dbname))
+    url = convertDSNToRfc1783(datasource, args.dbname)
+    engine = create_engine(url)
     input = pd.read_sql(
         sql=select_input,
         con=engine)
