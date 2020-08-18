bd314ad6ef4b3c6c460b46c2d7349243b7f02d8d
brightcoder01 <bright.gong@outlook.com>
2020-08-18 15:57:01 +0800

    Add lag columns before rolling time series data and extract features.

--- runnables/extract_ts_features.py
+++ runnables/extract_ts_features.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd 
 from run_io.db_adapter import convertDSNToRfc1783
 from sqlalchemy import create_engine
-from time_series_processing.ts_feature_extractor import add_features_extracted_from_ts_data
+from time_series_processing.ts_feature_extractor import add_features_extracted_from_ts_data, add_lag_columns
 
 
 def build_argument_parser():
@@ -12,6 +12,7 @@ def build_argument_parser():
     parser.add_argument("--column_id", type=str, required=True)
     parser.add_argument("--column_time", type=str, required=True)
     parser.add_argument("--columns_value", type=str, required=True)
+    parser.add_argument("--lag_num", type=int, default=1)
     parser.add_argument("--windows", type=str, required=True)
     parser.add_argument("--min_window", type=str, default=0)
     parser.add_argument("--extract_setting", type=str, default="minimal", choices=["minimal", "efficient", "comprehensive"])
@@ -35,16 +36,20 @@ if __name__ == "__main__":
         sql=select_input,
         con=engine)
 
+    df_with_lag_columns, lag_column_names = add_lag_columns(input, columns_value, args.lag_num)
+
     print("Start extracting the features from the time series data.")
     df_with_extracted_features = add_features_extracted_from_ts_data(
-        input,
+        df_with_lag_columns,
         column_id=args.column_id,
         column_time=args.column_time,
-        columns_value=columns_value,
+        columns_value=lag_column_names,
         windows=windows,
         min_window=args.min_window,
         extract_setting=args.extract_setting)
     print("Complete the feature extraction.")
+
+    df_with_extracted_features = df_with_extracted_features.drop(columns=lag_column_names)
 
     df_with_extracted_features.to_sql(
         name=output,
